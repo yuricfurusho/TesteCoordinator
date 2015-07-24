9bf3737ea6245712e38beee8b967d6746b4aa2a3
yuricfurusho <yuricfurusho@livetouch.com.br>
2015-07-24 17:48:05 -0300

    up

--- mobile/src/main/java/yuricfurusho/testecoordinator/MainActivity.java
+++ mobile/src/main/java/yuricfurusho/testecoordinator/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -20,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_collapsing);
 
         res = getResources();
 
